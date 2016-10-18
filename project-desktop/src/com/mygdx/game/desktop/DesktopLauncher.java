963805b176ff4d30f848df9ec0e8468374d5e94c
Chatchai Wongdetsakul <it58070020@office365.it.kmitl.ac.th>
2016-10-19 00:49:10 +0700

    Create Project

---
+++ project-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 320;
+		config.height = 320;
+		config.title = "Road to A";
+		new LwjglApplication(new MyGame(), config);
+	}
+}
